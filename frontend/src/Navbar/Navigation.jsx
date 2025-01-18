a5c39e525c1b8e656851cd0724b7244cf5fa2074
Betojr04 <betomasia12@gmail.com>
2025-01-17 18:47:01 -0700

    finished the responsive navbar with off canvas pop up for the nav menu on smaller menus

--- frontend/src/Navbar/Navigation.jsx
+++ frontend/src/Navbar/Navigation.jsx
@@ -3,19 +3,32 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from 'react-bootstrap/Nav';
 import "./Navigation.css";
+import Offcanvas from 'react-bootstrap/Offcanvas'
 
 const Navigation = () => {
   return (
     <>
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar bg="light" expand="lg" >
         <Container className="ms-auto">
-          <Navbar.Brand href="#">navbar</Navbar.Brand>
-          <Nav className="me-auto">
+          <Navbar.Brand href="#">Marvel Heros</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Offcanvas id="offcanvasnavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+          placement="end" >
+          <Offcanvas.Header closeButton >
+            <Offcanvas.Title className="offcanvasNavbarlabel" >
+             Menu
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+          <Nav className="ms-auto">
             <Nav.Link href="#">Home</Nav.Link>
             <Nav.Link href="#">Home</Nav.Link>
             <Nav.Link href="#">Home</Nav.Link>
             <Nav.Link href="#">Home</Nav.Link>
           </Nav>
+          </Offcanvas.Body>
+          </Navbar.Offcanvas>
         </Container>
       </Navbar>
       </> 
